3c9360fb2733faee3eb4fae4d88d9a6b9f4d88a9
Ondřej Švanda <46406259+Papooch@users.noreply.github.com>
2022-06-09 14:02:45 +0200

    initial commit

---
+++ src/main/kotlin/cz/papooch/spring_kotlin_exchange/SpringKotlinExchangeApplication.kt
@@ -0,0 +1,11 @@
+package cz.papooch.spring_kotlin_exchange
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinExchangeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinExchangeApplication>(*args)
+}
---
+++ src/test/kotlin/cz/papooch/spring_kotlin_exchange/SpringKotlinExchangepplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.papooch.spring_kotlin_exchange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinExchangeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
